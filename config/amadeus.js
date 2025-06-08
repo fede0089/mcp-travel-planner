46bcff22b01cc21932eff5a3d3cfb2e85cd65b90
Federico Mete <>
2025-06-08 11:35:14 -0300

    refactor: remove debug logs from amadeus config

--- config/amadeus.js
+++ config/amadeus.js
@@ -13,11 +13,6 @@ const clientSecret =
     ? process.env.AMADEUS_PROD_CLIENT_SECRET
     : process.env.AMADEUS_TEST_CLIENT_SECRET;
 
-// Logs temporales para debug
-console.log("Entorno:", env);
-console.log("Client ID:", clientId?.substring(0, 4) + "...");
-console.log("Client Secret:", clientSecret?.substring(0, 4) + "...");
-
 if (!clientId || !clientSecret) {
   throw new Error(
     `Las credenciales de Amadeus para el entorno ${env} no están configuradas en el archivo .env`
